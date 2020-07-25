8a835877ec7ccc4798c5b13224cb1b40525c5c3f
Sakshi Chaurasiya <56647481+sakshichaurasiya123@users.noreply.github.com>
2020-07-26 00:22:31 +0530

    Create differencebetweenString&Stringbuffer.java

---
+++ differencebetweenString&Stringbuffer.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String a=new String("Rani");
+        a.concat("Chaurasiya")
+        System.out.println(a);
+        
+        StringBuffer ab=new StringBuffer("Rani");
+        ab.append("Chaurasiya");
+        System.out.println(ab);
+       
+     }
+}
