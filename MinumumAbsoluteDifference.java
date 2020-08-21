57187d68d69967fcfbd7b35cede112ab00e41753
Sakshi Chaurasiya <56647481+sakshichaurasiya123@users.noreply.github.com>
2020-08-21 14:41:55 +0530

    Create MinumumAbsoluteDifference.java

---
+++ MinumumAbsoluteDifference.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the substrCount function below.
+    static long substrCount(int n, String s) {
+        ArrayList<Character> ch=new ArrayList<Character>();
+        char arr[]=s.toCharArray();
+        for(int i=0; i<arr.length; i++)
+                ch.add(arr[i]);
+        System.out.println(ch);
+        return n
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String s = scanner.nextLine();
+
+        long result = substrCount(n, s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
