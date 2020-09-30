7f579a29330a15f71fa3bd1b8e5d99bf3aba3fc0
Sakshi Chaurasiya <56647481+sakshichaurasiya123@users.noreply.github.com>
2020-10-01 00:40:05 +0530

    Create SubArrayWithGivenSumSlidingWIndow.java

---
+++ SubArrayWithGivenSumSlidingWIndow.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+	Scanner in=new Scanner(System.in);
+	int n=in.nextInt();
+	int arr[]=new int[n];
+	for(int i=0; i<n; i++)
+	    arr[i]=in.nextInt();
+	 int k=in.nextInt();
+	    minimum(arr,n,k);
+	    
+   	}
+   	//Brute Force Solution
+   	/*static void minimum(int arr[],int n,int k){
+   	    int i=0;
+   	    int max=0;
+   	    int diff=0;
+   	    while(i<(n-k)){
+   	        
+   	        for(int j=i; j<k; j++){
+   	            max +=arr[j];
+   	        }
+   	        i++;
+   	        diff=Math.max(diff,max);
+   	    }
+   	    System.out.println(diff);
+   	}*/
+   	//Sliding Window Technique
+   	static void minimum(int arr[],int n,int k){
+   	    int wsum=0;
+   	    int max=0;
+   	    int j=0;
+   	    int start=0,end=0;
+   	    StringBuffer br=new StringBuffer();
+   	    for(int i=0; i<n; i++){
+   	       wsum +=arr[i];
+   	       br.append(arr[i]).append(" ");
+   	       while(wsum>k && start<=i){
+   	            wsum =wsum-arr[start];
+   	            br.deleteCharAt(start);
+   	            start++;
+   	        
+   	        }
+   	       if(wsum==k){
+   	        System.out.println(start+" "+i+"  sum : "+wsum);
+   	        System.out.println(br);
+   	        return;
+   	       }
+   	        
+   	        
+   	    }
+   	   	}
+}
